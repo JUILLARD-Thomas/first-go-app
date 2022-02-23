267a46dfe82dfd82989593d591703530c20d381a
tjuillar <thomasjuillard@orange.fr>
2022-02-23 17:10:20 +0100

    chore: :rocket: dockerise go app

--- main.go
+++ main.go
@@ -16,5 +16,5 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message":"hello gogo!"}`))
+	w.Write([]byte(`{"message":"hello Thomas! How are you today ?", "response":"I'm fine."}`))
 }
