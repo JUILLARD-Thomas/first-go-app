297d4073fce9f0108febe3f4b2e5e72e20b06d6d
tjuillar <thomasjuillard@orange.fr>
2022-03-27 21:16:57 +0200

    feat: :sparkles:

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "postgres"
+	port     = 5432
+	user     = "user"
+	password = "password"
+	dbname   = "user"
 )
 
 func main() {
 	http.HandleFunc("/", hello)
 	fmt.Println("Server started")
+
+	// connStr := fmt.Sprintf("user=%s dbname=%s sslmode=disable", user, dbname)
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s  sslmode=disable",
+		host, port, user, password)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	fmt.Println("Successfully connected:", db.Ping() == nil)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
